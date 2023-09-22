559c319b143267ac8788bc35f3d8dc7031107e8e
Levente Nagy <113554470+Levkol@users.noreply.github.com>
2023-09-22 13:58:53 +0200

    The Array reduce Method

--- 02-JS-review/starter/script.js
+++ 02-JS-review/starter/script.js
@@ -292,3 +292,7 @@ const adventureBooks = books
   .filter((books) => books.genres.includes("adventure"))
   .map((book) => book.title);
 adventureBooks;
+
+// The Array reduce Method
+const pagesAllBooks = books.reduce((sum, book) => sum + book.pages, 0);
+pagesAllBooks;
