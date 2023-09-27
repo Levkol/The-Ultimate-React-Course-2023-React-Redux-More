2f8071ca30ffc583e6cf9b32bffc0521c42b5590
Levente Nagy <113554470+Levkol@users.noreply.github.com>
2023-09-27 12:00:31 +0200

    coding-challenge-v2

---
+++ Coding Challenges/coding-challenege-v2/src/index.js
@@ -0,0 +1,105 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+
+const skills = [
+  {
+    skill: "HTML + CSS",
+    level: "advanced",
+    color: "#2662EA",
+  },
+  {
+    skill: "JavaScript",
+    level: "advanced",
+    color: "#EFD81D",
+  },
+  {
+    skill: "Web Design",
+    level: "advanced",
+    color: "#C3DCAF",
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33",
+  },
+  {
+    skill: "React",
+    level: "beginner",
+    color: "#60DAFB",
+  },
+];
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <img className="avatar" src="avatar/levente.jpg" alt="Developer avatar" />
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Levente Nagy</h1>
+      <p>
+        Full-stack web developer. Beyond coding, you'll often find me engrossed
+        in a variety of passions, including engaging in exhilarating coding
+        projects, immersing myself in the world of finance through extensive
+        reading, staying at the forefront of technology with a keen interest in
+        web3 technologies, and honing my physical and mental prowess through
+        dedicated training sessions at the gym.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      {skills.map((skill) => (
+        <Skill
+          skill={skill.skill}
+          color={skill.color}
+          level={skill.level}
+          key={skill.skill}
+        />
+      ))}
+    </div>
+  );
+}
+
+function Skill({ skill, color, level }) {
+  return (
+    <div className="skill" style={{ backgroundColor: color }}>
+      <span>{skill}</span>
+      <span>
+        {level === "beginner" && "👶"}
+        {level === "intermediate" && "👍"}
+        {level === "advanced" && "💪"}
+      </span>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
