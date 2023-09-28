3ff91cbd18d81db4df04cbc9db0866b4e72b431d
Levente Nagy <113554470+Levkol@users.noreply.github.com>
2023-09-28 07:02:40 +0200

    Handling Events the React Way

--- 04-steps/starter/src/App.js
+++ 04-steps/starter/src/App.js
@@ -7,6 +7,14 @@ const messages = [
 export default function App() {
   const step = 3;
 
+  function handlePrevious() {
+    alert("Previous");
+  }
+
+  function handleNext() {
+    alert("Next");
+  }
+
   return (
     <div className="steps">
       <div className="numbers">
@@ -20,10 +28,17 @@ export default function App() {
       </p>
 
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+          // onMouseEnter={() => alert("Hover")}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
           Next
         </button>{" "}
       </div>
