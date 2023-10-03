42d9db45235a69352eb0839ef95639df8da2f047
Levente Nagy <113554470+Levkol@users.noreply.github.com>
2023-10-03 12:08:08 +0200

    A Vanilla JS Comparison and Building a More Complex State Tree

--- 04-steps/starter/src/App.js
+++ 04-steps/starter/src/App.js
@@ -7,6 +7,15 @@ const messages = [
 ];
 
 export default function App() {
+  return (
+    <div>
+      <Steps />
+      <Steps />
+    </div>
+  );
+}
+
+function Steps() {
   const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(true);
   // const [test, setTest] = useState({ name: "Jonas" });
@@ -28,7 +37,7 @@ export default function App() {
   }
 
   return (
-    <>
+    <div>
       <button className="close" onClick={() => setIsOpen((is) => !is)}>
         &times;
       </button>
@@ -63,6 +72,6 @@ export default function App() {
           </div>
         </div>
       )}
-    </>
+    </div>
   );
 }
