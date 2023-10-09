82de411536503a2980d1b0d5694a13c735a33bf7
Levente Nagy <113554470+Levkol@users.noreply.github.com>
2023-10-09 11:09:12 +0200

    Building the Layout

---
+++ 05-travel-list/starter/src/App.js
@@ -0,0 +1,34 @@
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />,
+      <From />,
+      <PackingList />,
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🏝️ Far Away 💼</h1>;
+}
+
+function From() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+
+function Stats() {
+  return (
+    <footer>
+      <em>💼 You have X item on your list, and you already packed X (X%) </em>
+    </footer>
+  );
+}
